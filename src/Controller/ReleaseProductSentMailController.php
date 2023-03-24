1350c568ebad6443e8315911323c73225677f5c5
saurav-1907 <100284436+saurav-1907@users.noreply.github.com>
2023-03-24 15:48:18 +0530

    Update ReleaseProductSentMailController.php

--- src/Controller/ReleaseProductSentMailController.php
+++ src/Controller/ReleaseProductSentMailController.php
@@ -77,42 +77,42 @@ class ReleaseProductSentMailController extends AbstractController
         $displayPrice = 0;
         if ($products) {
             foreach ($subscriberCustomers as $subscriberCustomer) {
-               $customerIds[] = $subscriberCustomer->getId();
-               $customerData = $this->getCustomerGroupForPrice($subscriberCustomer, $context);
-               foreach ($customerData as $data) {
+                $customerIds[] = $subscriberCustomer->getId();
+                $customerData = $this->getCustomerGroupForPrice($subscriberCustomer, $context);
+                foreach ($customerData as $data) {
                    $displayPrice = $data->getGroup()->getDisplayGross();
-               }
-               $salesChannelId = $subscriberCustomer->getSalesChannelId();
-               $salesChannelNames = $this->getSalesChannelName($salesChannelId, $context);
-               $salesChannelName = '';
-               foreach ($salesChannelNames as $salesChannelName) {
+                }
+                $salesChannelId = $subscriberCustomer->getSalesChannelId();
+                $salesChannelNames = $this->getSalesChannelName($salesChannelId, $context);
+                $salesChannelName = '';
+                foreach ($salesChannelNames as $salesChannelName) {
                    $salesChannelName = $salesChannelName->getName();
-               }
-               $releaseProductDetails = array();
-               $releaseProductDetails['salesChannelId'] = $subscriberCustomer->getSalesChannelId();
-               $releaseProductDetails['salesChannelName'] = $salesChannelName;
-               $releaseProductDetails['firstName'] = $subscriberCustomer->getFirstName();
-               $releaseProductDetails['lastName'] = $subscriberCustomer->getLastName();
-               $releaseProductDetails['email'] = $subscriberCustomer->getEmail();
-               $releaseProductDetails['displayPrice'] = $displayPrice;
-               $releaseProductInfoData[] = $releaseProductDetails;
+                }
+                $releaseProductDetails = array();
+                $releaseProductDetails['salesChannelId'] = $subscriberCustomer->getSalesChannelId();
+                $releaseProductDetails['salesChannelName'] = $salesChannelName;
+                $releaseProductDetails['firstName'] = $subscriberCustomer->getFirstName();
+                $releaseProductDetails['lastName'] = $subscriberCustomer->getLastName();
+                $releaseProductDetails['email'] = $subscriberCustomer->getEmail();
+                $releaseProductDetails['displayPrice'] = $displayPrice;
+                $releaseProductInfoData[] = $releaseProductDetails;
            }
             foreach ($releaseProductInfoData as $email) {
-               foreach ($products as $product) {
-                $productId = $product->getId();
-               }
-               $checkLog = $this->checkEntryExistOrNot($productId, $context);
-               $id = $checkLog->getTotal() == 0 ? Uuid::randomHex():$checkLog->first()->getId();
-               $uniquecustomerIds = array_unique($customerIds);
-               $this->emailService->sendEmail($email, $products, Context::createDefaultContext());
-               $this->releaseProductRepository->upsert([
+                foreach ($products as $product) {
+                    $productId = $product->getId();
+                }
+                $checkLog = $this->checkEntryExistOrNot($productId, $context);
+                $id = $checkLog->getTotal() == 0 ? Uuid::randomHex():$checkLog->first()->getId();
+                $uniquecustomerIds = array_unique($customerIds);
+                $this->emailService->sendEmail($email, $products, Context::createDefaultContext());
+                $this->releaseProductRepository->upsert([
                    [
                        'id' => $id,
                        'productId' => $productId,
                        'value' => $uniquecustomerIds,
                        'lastUsageAt'=> date("Y-m-d"),
                    ]
-               ], $context);
+                ], $context);
            }
            dump("hello");
            return new JsonResponse([
